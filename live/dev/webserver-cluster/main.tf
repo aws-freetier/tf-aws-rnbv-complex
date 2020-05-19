efebeb6fe77c3de4ccf63a5f97bdd302fde63644
baranovrp <baranov.r.p@gmail.com>
2020-05-19 21:03:52 +0200

    TFAWSORG-01 Reduce asg group

--- live/dev/webserver-cluster/main.tf
+++ live/dev/webserver-cluster/main.tf
@@ -68,7 +68,7 @@ module "asg" {
   db_remote_state_bucket = "tf-state-eu-west-2-rnbv"
   db_remote_state_key    = "dev/services/webserver-cluster/terraform.tfstate"
 
-  min_size             = 2
+  min_size             = 1
   max_size             = 3
   image_id             = data.aws_ami.ami2.id
   instance_type        = local.instance_type
