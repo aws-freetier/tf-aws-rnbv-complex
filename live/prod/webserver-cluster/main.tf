a59780ecf29c3620709a839aa5056284dfda40ce
baranovRP <baranov.r.p@gmail.com>
2020-06-02 12:26:11 +0100

    Update main.tf

--- live/prod/webserver-cluster/main.tf
+++ live/prod/webserver-cluster/main.tf
@@ -5,7 +5,8 @@ provider "aws" {
 }
 
 // for demo purpose
-data "aws_ami" "ubuntu" {
+/*
+  data "aws_ami" "ubuntu" {
   most_recent = true
 
   filter {
@@ -34,4 +35,4 @@ resource "aws_instance" "secondserver" {
   }
   subnet_id = data.aws_subnet.default.id
 }
-
+*/
