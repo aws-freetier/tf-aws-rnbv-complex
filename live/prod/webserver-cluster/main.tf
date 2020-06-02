99e32b09687201a7862399175fbe70a16a8e4202
baranovRP <baranov.r.p@gmail.com>
2020-06-02 12:34:44 +0100

    Demo shutdown instance

--- live/prod/webserver-cluster/main.tf
+++ live/prod/webserver-cluster/main.tf
@@ -5,7 +5,7 @@ provider "aws" {
 }
 
 // for demo purpose
-
+/*
 data "aws_ami" "ubuntu" {
   most_recent = true
 
@@ -35,3 +35,4 @@ resource "aws_instance" "secondserver" {
   }
   subnet_id = data.aws_subnet.default.id
 }
+*/
